d438ae8f68b2a78e346141b8c934cf82910edf07
TaiefSiddique <taiefsiddik2@gmail.com>
2025-04-12 09:58:00 +0600

    dep

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running in development mode on port ${PORT}`);
